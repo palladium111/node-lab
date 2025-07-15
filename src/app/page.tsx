cecfc8cd928aa520061020a60f3e8747716ab295
Bryan Nelson <btnelson@gmail.com>
2025-07-15 19:50:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import dynamic from 'next/dynamic'
-import { Skeleton } from '@/components/ui/skeleton'
-
-const NodeWeaverApp = dynamic(() => import('@/components/node-weaver-app').then(mod => mod.NodeWeaverApp), {
-  ssr: false,
-  loading: () => <div className="w-screen h-screen flex items-center justify-center bg-background"><p>Loading 3D Visualizer...</p></div>,
-})
+import NodeWeaverLoader from "@/components/node-weaver-loader";
 
 export default function Home() {
   return (
-    <NodeWeaverApp />
+    <NodeWeaverLoader />
   );
 }
---
+++ src/components/node-weaver-loader.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import dynamic from 'next/dynamic'
+
+const NodeWeaverApp = dynamic(() => import('@/components/node-weaver-app').then(mod => mod.NodeWeaverApp), {
+  ssr: false,
+  loading: () => <div className="w-screen h-screen flex items-center justify-center bg-background"><p>Loading 3D Visualizer...</p></div>,
+})
+
+export default function NodeWeaverLoader() {
+    return <NodeWeaverApp />;
+}
